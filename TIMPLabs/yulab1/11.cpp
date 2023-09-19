9fe69a8aa295f2cb47fa05327e76827d8b254513
Ulyana Yunusova <85567113+Ulyanaaa@users.noreply.github.com>
2023-09-19 11:35:28 +0300

    1

---
+++ TIMPLabs/yulab1/11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main(){
+    srand(1);
+
+    float rand_var = rand();
+    cout << "\n[Task1] Random variable: " << rand_var << "\n\n";
+}
