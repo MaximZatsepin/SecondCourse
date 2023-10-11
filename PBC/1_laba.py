00135023c8b3388581cc5329c56ca1976767db05
Maxim <130181963+wumpochuck@users.noreply.github.com>
2023-10-11 09:43:05 +0300

    1

--- PBC/1_laba.py
+++ PBC/1_laba.py
@@ -11,19 +11,19 @@ def result (x0, y0, r0, n):
     y_max = y0 + r0
 
     m = 0
-    D = random.randint(0, 100000)
+    D = 161/1000
     x = (x_max - x_min) * D + x_min
-    print(x)
-    D = random.randint(0, 100000)
+    # print(x)
+    D = 676/1000
     y = (y_max - y_min) * D + y_min
-    print(y)
-    for i in range(n):
-        
-        if ((x - x0**2) + (y - y0 ** 2)) <= r0**2:
-            m += 1
-
-    s = m/n *4
-    print(s)
+    # print(y)
+    # for i in range(n):
+    if (((x - x0)**2) + ((y - y0) ** 2)) <= r0**2:
+        m += 1
+    # print((((x - x0)**2) + ((y - y0) ** 2)))
+    # s = m/n *4
+    # print(s)
+    print(m)
 
 
 
