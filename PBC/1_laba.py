82e9f3a2612f6906af695ff91fcbceead934ee54
Maxim <130181963+wumpochuck@users.noreply.github.com>
2023-10-12 22:22:51 +0300

    1

--- PBC/1_laba.py
+++ PBC/1_laba.py
@@ -19,13 +19,27 @@ def result (x0, y0, r0, n):
     # print(y)
     # for i in range(n):
     if (((x - x0)**2) + ((y - y0) ** 2)) <= r0**2:
-        m += 1
+        # m += 1
+        print(1)
+    else:
+        print(0)
     # print((((x - x0)**2) + ((y - y0) ** 2)))
     # s = m/n *4
     # print(s)
-    print(m)
+    # print(m)
 
 
 
 
 result(x0, y0, r0, n = 1000)
+
+
+# a= 17
+# b = 1
+# M = 1000
+# x= 2
+
+# # mod [(a*x + b, M)] - формула линейного кконгруэтного генератора случайных чисел
+# for i in range(10):
+#     x = (a*x + 1) % M
+#     print(x)
