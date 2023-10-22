b231dfa543b153df3dac4538df1cb654b8b807c7
Maxim <130181963+wumpochuck@users.noreply.github.com>
2023-10-22 19:00:46 +0300

    1
    1

--- TIMPLabs/dop_timp/13.cpp
+++ TIMPLabs/dop_timp/13.cpp
@@ -1,27 +1,76 @@
-/*
-1.	Случайное выбранное число сохраняется в переменную. Тип переменной выбирается из таблицы типов переменных 
-*/
-
 #include <iostream>
-#include <time.h>
-
+#include <iomanip>
 
 using namespace std;
 
-//char
-int main()
-{
-    // Штука для серии
-    time_t t;
-    srand(time(&t));
+struct Point {
+    int x;
+    int y;
+    int dist; // Расстояние от начальной точки
+};
 
-    char randomChar = rand();
+const int ROWS = 8;
+const int COLS = 8;
 
-    // static_cast<int> - используется для явного преобразования значения переменной, из одиного типа данных в другой 
-    cout << "Random namber : " << static_cast<int>(randomChar) << endl;
+int grid[ROWS][COLS] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, -1, -1, 0, 0, -1, 0, 0},
+    {0, 0, 0, 0, -1, -1, 0, 0},
+    {0, -1, -1, 0, 0, 0, -1, 0},
+    {0, -1, 0, 0, -1, 0, -1, 0},
+    {0, 0, 0, 0, -1, 0, 0, 0},
+    {0, -1, -1, 0, 0, 0, -1, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+bool isValid(int x, int y) {
+    return x >= 0 && x < ROWS && y >= 0 && y < COLS && grid[x][y] == 0;
 }
 
+void waveAlgorithm(int startX, int startY, int endX, int endY) {
+    Point queue[ROWS * COLS];
+    int front = 0, rear = 1;
+    queue[front] = {startX, startY, 0};
 
+    while (front < rear) {
+        Point current = queue[front++];
+        
+        if (current.x == endX && current.y == endY) {
+            cout << "Кратчайшее расстояние: " << current.dist << endl;
+            return;
+        }
 
+        for (int i = 0; i < 4; i++) {
+            int newX = current.x + dx[i];
+            int newY = current.y + dy[i];
 
+            if (isValid(newX, newY)) {
+                grid[newX][newY] = current.dist + 1;
+                queue[rear++] = {newX, newY, current.dist + 1};
+            }
+        }
+    }
 
+    cout << "Путь не найден!" << endl;
+}
+
+int main() {
+    int startX = 0, startY = 0;
+    int endX = 7, endY = 7;
+
+    waveAlgorithm(startX, startY, endX, endY);
+
+    // Выводим результат на экран
+    cout << "Озеро с кратчайшими расстояниями:" << endl;
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            cout << setw(2) << grid[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
