3bbaa9dbc13ef8da28b20d966124f76bfcc17b9c
Maxim <130181963+wumpochuck@users.noreply.github.com>
2023-11-21 16:53:24 +0300

    1

--- TIMPLabs/other/zLab9/main.cpp
+++ TIMPLabs/other/zLab9/main.cpp
@@ -9,4 +9,48 @@
 
 */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+// #include "nlohmann/json.hpp"
+
+// using json = nlohmann::json;
+
+using namespace std;
+
+struct WorkType{
+    int idWork; string typeWork;
+    bool checkWorkType(){ return (!((this->idWork == 0) + (this->typeWork == ""))); }
+};
+
+struct CarModel{
+    int idCar; string nameCar;
+    bool checkCarModel(){ return (!((this->idCar == 0) + (this->nameCar == ""))); }
+};
+
+struct CarOwner{
+    int idOwner; string nameOwner; string passportOwner;
+    bool checkOwner(){ return (!((this->idOwner == 0)+(this->nameOwner == "")+(this->passportOwner == ""))); }
+};
+
+struct Car {
+    int idCar; string markCar; int idOwner; string numCar;
+    // bool checkCar(){return (!((this->idCar == 0)+(this->markCar == "")+(this->idOwner == 0)+(this->numCar = "")));}
+};
+
+struct Master{
+    int idMaster; string nameMaster; string passportMaster; string dateMaster;
+    // bool checkMaster(){return (!((this->idMaster == 0)+(this->nameMaster == "")+(this->passportMaster == "")+(this->dateMaster = "")));}
+};
+
+struct Work{
+    int idCar; int idWork; string startWork; string idMaster; string endWork; string descriptionWork;
+};
+
+int main(){
+    // ifstream f("data.json");
+    // json data = json::parse(f);
+    // cout << data;
+}
 
