a38350ecd0fb4f6a78d65ba61b67af58bad31675
Maxim <130181963+wumpochuck@users.noreply.github.com>
2024-02-02 13:48:16 +0300

    1

--- JavaLabs/Tester/src/Main.java
+++ JavaLabs/Tester/src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+import java.util.List;
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int[][] matrix = new int[5][5];
+        matrix[2][3] = 2;
+        System.out.print("Введите ваше имя: ");
+        String stroke = scanner.nextLine();
+        System.out.println("Hello, " + stroke );
+        /*
+        System.out.println(2+3);
+        System.out.println(2-3);
+        System.out.println(2 / 3);
+        System.out.println(2 % 3);
+        System.out.println(Math.pow(2,3));
+        System.out.println((int)Math.sqrt(4));
+        */
+
+        String[] stroke2 = scanner.nextLine().split(" ");
+        List<Integer> array = new ArrayList<>();
+        for(String i : stroke2){
+            array.add(Integer.parseInt(i));
+        }
+        System.out.print("Ваш массив:\n");
+        for(Integer i : array){
             System.out.println("i = " + i);
         }
+
     }
 }
+
+
+
