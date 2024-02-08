c38fedca0d555dee844c1b59df980e33d0067934
Maxim <130181963+wumpochuck@users.noreply.github.com>
2024-02-08 10:12:49 +0300

    1

--- JavaLabs/zLab1/src/Palindrome.java
+++ JavaLabs/zLab1/src/Palindrome.java
@@ -1,18 +1,26 @@
 public class Palindrome {
+    // java Palindrome madam racecar apple kayak song noon
     public static void main(String[] args){
         for(int i = 0; i < args.length; i++){
-            String str = args[i];
+            String s = args[i];
+            // Output
+            if(isPalindrome(s)) System.out.println(s + " is Palindrome");
+            else System.out.println(s + " is not Palindrome");
         }
     }
-    public static String reverseString(String str){
-        String reverse_str = "";
-        for(int i = str.length() - 1; i >= 0; i--){
-            reverse_str += str.charAt(i);
+    // Reversing string
+    public static String reverseString(String s){ 
+        String reverse_s = "";
+        for(int i = s.length() - 1; i >= 0; i--){
+            reverse_s += s.charAt(i);
         }
-        return reverse_str;
+        return reverse_s;
     }
-    public static boolean isPalindrome(String str){
-        String str1 = str;
-        String str2 = reverseString(str);
+    // Checking string for Palindrome
+    public static boolean isPalindrome(String s){
+        String s1 = s;
+        String s2 = reverseString(s);
+        if(s1.equals(s2)) return true;
+        return false;
     }
 }
--- JavaLabs/zLab1/src/Primes.java
+++ JavaLabs/zLab1/src/Primes.java
@@ -1,5 +1,6 @@
 public class Primes{
     public static void main(String[] args){
+        // Output
         System.out.println("Простые числа:");
         for(int i = 2; i <= 100; i++){
             if(isPrime(i)){
@@ -8,6 +9,7 @@ public class Primes{
         }
     }
 
+    // Checking is number Prime
     public static boolean isPrime(int n){
         for(int i = 2; i < n-1;  i++){
             if(n % i == 0) return false;
